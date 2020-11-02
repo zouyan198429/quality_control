a9dffa21322dc336ba6232a6b24c367ebaa4bcec
workservice <305463219@qq.com>
2020-11-02 13:20:44 +0800

    小吕修改样式

--- admin/resources/views/web/QualityControl/Market/layout_public/search.blade.php
+++ admin/resources/views/web/QualityControl/Market/layout_public/search.blade.php
@@ -10,8 +10,8 @@
             </div>
             <div class="bd">
                 <ul class="searchbox">
-                    <input type="radio" name="company_field" value="company_name" id="killOrder1"  @if (isset($field) && ($field == 'company_name' || $field != 'company_credit_code') ) checked @endif><label for="killOrder1">检验机构名称</label>
-                    <input type="radio" name="company_field" value="company_credit_code" id="killOrder2"   @if (isset($field) && $field == 'company_credit_code') checked @endif><label for="killOrder2">统一社会信用代码或组织机构代码</label>
+                    <div style="display: none;"> <input type="radio" name="company_field" value="company_name" id="killOrder1"  @if (isset($field) && ($field == 'company_name' || $field != 'company_credit_code') ) checked @endif><label for="killOrder1">检验机构名称</label>
+                    <input type="radio" name="company_field" value="company_credit_code" id="killOrder2"   @if (isset($field) && $field == 'company_credit_code') checked @endif><label for="killOrder2">统一社会信用代码或组织机构代码</label></div>
                     <input type="text" name="keyword" placeholder="" class="inp"   @if(isset($field) && $qkey == 1  && ($field == 'company_name' || $field == 'company_credit_code'))  value="{{ $keyword ?? '' }}"  @endif ><button class="searchbtn searchbtn_company" >搜索</button>
                 </ul>
                 <ul class="searchbox">
